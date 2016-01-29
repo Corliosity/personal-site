5ba6d7a7900fa37c5666e223a4b8487cb77a31a3
Corliosity <andrew.corliss@gmail.com>
2016-01-28 21:37:17 -0700

    Total Update

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 var express = require('express'),
-	path = require('path');
+	path = require('path'),
+	http = require('http'),
+	https = require('https'),
+	route = require('./router'),
+	port = 3001;
 var app = express();
 
+app.set('view engine', 'jade');
+app.set('view cache', false);
 
-app.get('/', function(req, res) {
-	res.send('Hello World');
-});
+app.set('views', __dirname + '/source');
+app.use('/assets', express.static(__dirname + '/source/assets/'));
 
-app.listen(port, function(){
-	console.log('Express App Server listening on porth %d in %s mode', port, app.settings.env);
-});
+app.get('/', route);
+
+http.createServer(app).listen(port);
+// https.createServer(options, app).listen(443);
