a4f1d26bd8e6861da21d4ec9143e2553e4f69a58
Corliosity <andrew.corliss@gmail.com>
2016-01-30 20:06:31 -0700

    Creating layout of the site using single page design

--- router.js
+++ router.js
@@ -7,8 +7,36 @@ router.use(function timeLog(req, res, next) {
   next();
 });
 
+//['Desgin', 'SASS/CSS', 'Back-End', 'JavaScript']
 router.get('/', function(req, res) {
-	res.render('index.jade');
+	var skillsList = [
+		{
+			id: 1,
+			title: 'UX Design',
+			desc: 'My experiences, trails and tripulation with creating an interface for users.',
+			buttonTitle: 'See Detail'
+		},
+		{
+			id: 2,
+			title: 'SASS/CSS',
+			desc: 'UX being transfered to the page in an understanding of data, and user needs.',
+			buttonTitle: 'Example'
+		},
+		{
+			id: 3,
+			title: 'Back-End',
+			desc: 'Take a peek at what happens behind the scenes or my explorations in Node.',
+			buttonTitle: 'Work and Code'
+		},
+		{
+			id: 4,
+			title: 'JavaScript',
+			desc: 'Java != JavaScript.\nNow that is over let us talk about the role of JS in development.',
+			buttonTitle: 'See More'
+		}
+	]
+
+	res.render('index.jade', {skillList: skillsList});
 });
 
 module.exports = router;
